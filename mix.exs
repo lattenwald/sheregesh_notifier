a3af8d9e2fcfb09ec6e757c211fea60f26af9c57
Alexander Q <qalexx@gmail.com>
2017-03-24 18:26:01 +0300

    setting up edeliver

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule Sheregesh.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [{:distillery, "~> 1.2"},
+     {:edeliver, "~> 1.4"}]
   end
 end
