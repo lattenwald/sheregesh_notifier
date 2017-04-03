d954c1668257d36611e9bd7bb68952f1e3c71ee2
Alexander Q <qalexx@gmail.com>
2017-04-03 19:06:19 +0300

    before testing we remove dets file

--- apps/tgchats/test/test_helper.exs
+++ apps/tgchats/test/test_helper.exs
@@ -1,3 +1,5 @@
-Application.put_env(:tgchats, :table, :tgchats_test)
+table = :tgchats_test
+table |> Atom.to_string |> File.rm
+Application.put_env(:tgchats, :table, table)
 Application.ensure_all_started(:tgchats)
 ExUnit.start()
