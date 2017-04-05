afb921be1d39780fc110be6fbe685fb91d5e57cd
Alexander Q <qalexx@gmail.com>
2017-04-05 17:14:58 +0300

    more umbrella stuff

--- apps/checker/mix.exs
+++ apps/checker/mix.exs
@@ -3,7 +3,7 @@ defmodule Checker.Mixfile do
 
   def project do
     [app: :checker,
-     version: append_revision("0.1.1"),
+     version: append_revision("0.1.2"),
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
