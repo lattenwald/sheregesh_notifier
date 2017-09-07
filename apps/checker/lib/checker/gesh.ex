fc76904c6ea44ff6580df22cca3cfeb7fcd8e1f2
Alexander Q <qalexx@gmail.com>
2017-09-07 19:37:32 +0300

    fixed failing parsing of date in header on gesh.info

--- apps/checker/lib/checker/gesh.ex
+++ apps/checker/lib/checker/gesh.ex
@@ -4,13 +4,16 @@ defmodule Checker.Gesh do
   def fetch() do
     Logger.info "#{__MODULE__} fetching data"
     with {:ok, 200, _headers, client} <- :hackney.request(:get, "http://gesh.info/svodki", [], "", []),
-         {:ok, body} <- :hackney.body(client) do
-      latest = Regex.named_captures(
-        ~r{<td class="td-center-news">(?<content><a href="http://gesh.info/svodki/.*?><strong>(?<date>.*?)</strong>.*?)</td>}m,
-        body)
-      {:ok, date} = Map.fetch(latest, "date")
-      {:ok, data} = Map.fetch(latest, "content")
-      {:ok, data} = Pandex.html_to_markdown_strict(data)
+         {:ok, body} <- :hackney.body(client),
+         %{"date" => date, "content" => data} <- Regex.named_captures(
+           ~r{<td class="td-center-news">(?<content><a href="http://gesh.info/svodki/.*?><strong>(?<date>.*?)</strong>.*?)</td>}m,
+           body),
+         {:ok, data} <- Pandex.html_to_markdown_strict(data),
+         %{"day" => str_day, "month" => str_month, "year" => str_year} <- Regex.named_captures(~r/^(?<day>\d+)\s+(?<month>\S+)\s+(?<year>\d+)$/, date),
+         {day, ""} <- Integer.parse(str_day),
+         {:ok, month} <- str_to_month(str_month),
+         {year, ""} <- Integer.parse(str_year)
+      do
       data =
         data
         |> String.replace("**", "::::::")
@@ -21,25 +24,6 @@ defmodule Checker.Gesh do
         |> String.replace("\n\n", "::::::")
         |> String.replace("\n", " ")
         |> String.replace("::::::", "\n\n")
-
-      date_m = Regex.named_captures(~r/^(?<day>\d+)\s+(?<month>\S+)\s+(?<year>\d+)$/, date)
-      day = date_m["day"] |> String.to_integer
-      month = case date_m["month"] do
-                "январ" <> _ -> 1
-                "февра" <> _ -> 2
-                "март"  <> _ -> 3
-                "апрел" <> _ -> 4
-                "ма"    <> _ -> 5
-                "июн"   <> _ -> 6
-                "июл"   <> _ -> 7
-                "авгус" <> _ -> 8
-                "сентя" <> _ -> 9
-                "октяб" <> _ -> 10
-                "ноябр" <> _ -> 11
-                "декаб" <> _ -> 12
-              end
-      year = date_m["year"] |> String.to_integer
-      date = Timex.to_date({year, month, day})
 
       {date, data}
     else
@@ -52,4 +36,20 @@ defmodule Checker.Gesh do
   def start_link() do
     Checker.start_link("gesh.info", &fetch/0)
   end
+
+  ### helpers
+  defp str_to_month("январ" <> _), do: {:ok, 1}
+  defp str_to_month("февра" <> _), do: {:ok, 2}
+  defp str_to_month("март"  <> _), do: {:ok, 3}
+  defp str_to_month("апрел" <> _), do: {:ok, 4}
+  defp str_to_month("ма"    <> _), do: {:ok, 5}
+  defp str_to_month("июн"   <> _), do: {:ok, 6}
+  defp str_to_month("июл"   <> _), do: {:ok, 7}
+  defp str_to_month("авгус" <> _), do: {:ok, 8}
+  defp str_to_month("сентя" <> _), do: {:ok, 9}
+  defp str_to_month("октяб" <> _), do: {:ok, 10}
+  defp str_to_month("ноябр" <> _), do: {:ok, 11}
+  defp str_to_month("декаб" <> _), do: {:ok, 12}
+  defp str_to_month(_), do: :error
+
 end
